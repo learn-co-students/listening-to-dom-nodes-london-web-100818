3d9157c958b678623ba85133b06ec3c43accc8c4
congo-quinine <congo.quinine@gmail.com>
2018-11-26 08:47:26 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
+// 
+// const input = document.querySelector('input');
+//
+// input.addEventListener('keydown', function(e) {
+//   console.log(e.which);
+// });
+//
+//
+// const input = document.querySelector('input')
+//
+// input.addEventListener('keydown', function(e) {
+//   if (e.which === 71) {
+//     return e.preventDefault()
+//   } else {
+//     console.log(e.which)
+//   }
+// });
